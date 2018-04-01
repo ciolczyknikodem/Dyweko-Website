f9c8983d3781b800b1bbded9ac04996c8af4cdce
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-04-01 14:29:19 +0200

    Add contact page

---
+++ javascript/submit.js
@@ -0,0 +1,9 @@
+function submit_button() {
+    alert("Wiadomość została wysłana!");
+    document.getElementById("input-name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("subject").value="";
+    document.getElementById("message-content").value="";
+}
+
+function 
