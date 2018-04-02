8ba3f1ccb3ee6ebeb6382cbd03ecd2e6c982d795
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2018-04-02 15:59:51 +0200

    add little corrects of footer

--- javascript/submit.js
+++ javascript/submit.js
@@ -1,9 +1,7 @@
 function submit_button() {
-    alert("Wiadomość została wysłana!");
     document.getElementById("input-name").value="";
     document.getElementById("email").value="";
     document.getElementById("subject").value="";
     document.getElementById("message-content").value="";
+    window.alert("Wiadomość została wysłana!");
 }
-
-function 
